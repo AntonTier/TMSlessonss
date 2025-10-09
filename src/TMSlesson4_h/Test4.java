710c8119606452a48ac7e195bd65090e161c4eff
Anton Tierliukevich <antonterlukevic4@gmail.com>
2025-10-09 23:44:31 +0300

    second exercise done

--- src/TMSlesson4_h/Test4.java
+++ src/TMSlesson4_h/Test4.java
@@ -6,7 +6,8 @@ public class Test4 {
 
     public static void main(String[] args) {
 
-        printArrayBothWays();
+        //printArrayBothWays();
+        SearchMaxMin();
 
     }
 
@@ -41,6 +42,58 @@ public class Test4 {
             System.out.print(array[i] + " ");
 
         }
+
+    }
+
+    public static void SearchMaxMin() {
+
+        Scanner input  = new Scanner(System.in);
+        System.out.print("Введите размер массива: ");
+        int size = input.nextInt();
+        int [] array = new int[size];
+
+        System.out.println();
+
+        for (int i=0;i<=array.length-1;i++) {
+
+           array[i] = (int) (Math.random()*100);
+
+        }
+
+        System.out.println("Ваш массив: ");
+        for (int i=0;i<=array.length-1;i++) {
+
+            System.out.print(array[i] + " ");
+
+        }
+
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 0;i<=array.length-1;i++) {
+
+            if (array[i]>max) {
+
+                max=array[i];
+
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("Максимальный элемент: " + max);
+
+        for (int i = 0;i<=array.length-1;i++) {
+
+            if (array[i]<min) {
+
+                min=array[i];
+
+            }
+
+        }
+
+        System.out.println("Минимальный элемнет: " + min);
 
     }
 
