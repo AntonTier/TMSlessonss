3a955381fe78b6a6a2fff206fcf61566ce618ca9
Anton Tierliukevich <antonterlukevic4@gmail.com>
2025-10-09 18:43:30 +0300

    third homework done

--- src/TMSlesson3_h/Test3.java
+++ src/TMSlesson3_h/Test3.java
@@ -1,4 +1,106 @@
 package TMSlesson3_h;
 
+import java.util.Scanner;
+
 public class Test3 {
+
+    public static void main(String[] args) {
+
+        //isEven();
+        //getTemperature();
+        //PrintSquares();
+        //SequencePrinter();
+        SumRange();
+
+    }
+
+    public static void isEven () {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = input.nextInt();
+
+        if (a%2==0) {
+
+            System.out.println("Число " + a + " четное");
+
+        }
+        else {
+
+            System.out.println("Число " + a + " нечетное");
+
+        }
+
+        input.close();
+
+    }
+
+    public static void getTemperature() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите температуру: ");
+        int t = input.nextInt();
+
+        if (t>-5) {
+
+            System.out.println("Warm");
+
+        }
+        else if(t<=-5 && t>-20) {
+
+            System.out.println("Normal");
+
+        }
+        else if(t<=-20) {
+
+            System.out.println("Cold");
+
+        }
+
+    }
+
+    public static void PrintSquares() {
+
+        for (int i = 10;i<=20;i++) {
+
+            System.out.println("Квадрат числа " + i + " равен " + i*i);
+
+        }
+
+    }
+
+    public static void SequencePrinter() {
+
+        for (int i = 7;i<=98;i+=7) {
+
+            System.out.print(i + " ");
+
+        }
+
+    }
+
+    public static void SumRange() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int a = input.nextInt();
+        if (a<=0) {
+
+            System.out.println("Введите число больше 0!!!");
+
+        }
+
+        int sum = 0;
+
+        for (int i = 0;i<=a;i++) {
+
+            sum+=i;
+
+        }
+
+        System.out.println("Сумма от 1 до " + a + " равна " + sum);
+
+    }
+
+
 }
