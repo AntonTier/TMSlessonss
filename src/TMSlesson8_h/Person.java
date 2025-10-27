a9154234438c68befb2ded3fbcf7e7f367152bf8
Anton Tierliukevich <antonterlukevic4@gmail.com>
2025-10-27 18:11:47 +0300

    8.2 homework done

---
+++ src/TMSlesson8_h/Person.java
@@ -0,0 +1,13 @@
+package TMSlesson8_h;
+
+public class Person {
+
+    static final String name = "Anton";
+
+    static void infName() {
+
+        System.out.println(name);
+
+    }
+
+}
---
+++ src/TMSlesson8_h/Test8.java
@@ -0,0 +1,12 @@
+package TMSlesson8_h;
+
+public class Test8 {
+
+    public static void main(String[] args) {
+
+        System.out.println(Person.name);
+        Person.infName();
+
+    }
+
+}
