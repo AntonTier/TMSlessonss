9858bca3f87d4d0427153953319dbc723229d7e2
Anton Tierliukevich <antonterlukevic4@gmail.com>
2025-11-06 22:47:33 +0300

    9.1.1 homework done

--- src/TMSlesson9_h/Circle.java
+++ src/TMSlesson9_h/Circle.java
@@ -1,7 +1,69 @@
-/*package TMSlesson9_h;
+package TMSlesson9_h;
+
+import java.util.Scanner;
 
 public class Circle extends Figure {
 
+    public void Perimeter() {
 
+        System.out.println("Вычисление периметра Круга");
+        Scanner s = new Scanner(System.in);
+        int R;
 
-}*/
+        while (true) {
+
+            System.out.print("Введите радиус круга: ");
+            R = s.nextInt();
+
+            if (R>=0) {
+
+                break;
+
+            }
+            else {
+
+                System.out.println("Введите корректные значения");
+                continue;
+
+            }
+
+        }
+        System.out.println();
+
+        System.out.println("Длина окружности с радуисом " + R + " равен " + (2*3.14*R));
+        s.close();
+
+    }
+
+    public void Square() {
+
+        System.out.println("Вычисление площади круга");
+        Scanner s = new Scanner(System.in);
+        int R;
+
+        while (true) {
+
+            System.out.print("Введите радиус окружности: ");
+            R = s.nextInt();
+
+            if (R>=0) {
+
+                break;
+
+            }
+            else {
+
+                System.out.println("Введите корректные значения");
+                continue;
+
+            }
+
+        }
+        System.out.println();
+
+        System.out.println("Площадь окружности с радиусом " + R + " равен " + (3.14*R*R));
+        s.close();
+
+    }
+
+}
